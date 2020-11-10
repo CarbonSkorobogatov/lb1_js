d539637f5b2d9ae8cbe4cf65e8a2232191373674
Zvat Scorobogatov <zvat.333333@gmail.com>
2020-11-10 14:49:26 +0200

    changing of code

--- navbar.js
+++ navbar.js
@@ -1,3 +1,13 @@
 function Click(){
+
     document.getElementById('droplist').classList.toggle("show");
+    document.body.classList.toggle('wrapper-back')
+
+    /*---------------------------------------------------------------------------*/ 
+    document.querySelector('.dropbtn').disabled = true;
+    document.querySelector('.dropbtn:hover').style.backgroundColor =  "#0a1925dc";
+    /*---------------------------------------------------------------------------*/ 
+
 }
+
+
