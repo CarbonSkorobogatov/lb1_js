6af1af55e52b21b9c1ac4df23e468bd238a3912e
Zvat Scorobogatov <zvat.333333@gmail.com>
2020-11-15 18:48:37 +0200

    some changing

--- navbar.js
+++ navbar.js
@@ -1,33 +1,30 @@
 let isclose = true;
-let  dropbtn = document.querySelector('.dropbtn');
-
 function Click(){
     Script();
-    dropbtn.disabled = true;
+    document.querySelector('.dropbtn').disabled = true;
     isclose = false; 
     Time();
 }
 
 function CloseClick(){
     Script();
-    dropbtn.disabled = false;
+    document.querySelector('.dropbtn').disabled = false;
 }
 
 
 function Script(){
-    document.querySelector('.droplist').classList.toggle("show");
-    document.body.classList.toggle('wrapper-back')
-    dropbtn.classList.toggle("dropbtn-off");
+    document.querySelector('.droplist').classList.toggle('show');
+    document.querySelector('.wrapper-off').classList.toggle('wrapper-on')
+    document.querySelector('.dropbtn').classList.toggle("dropbtn-off");
     isclose = true;
 }
 
 function Time(){
     setTimeout(function(){
-
         if(isclose == true){
         }else{
             Script();
-            dropbtn.disabled = false;
+            document.querySelector('.dropbtn').disabled = false;
         }
     }, 10000)
 }
